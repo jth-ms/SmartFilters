54adbd817a2d9b75f81ebb5cf22201f38061042b
Julia Taylor-Hell <juliata@microsoft.com>
2024-08-08 10:49:12 -0700

    Fix error with shader, was using input reserved word as a variable name

--- packages/demo/src/configuration/blocks/effects/tintBlock.fragment.glsl
+++ packages/demo/src/configuration/blocks/effects/tintBlock.fragment.glsl
@@ -4,7 +4,7 @@ uniform vec3 tintColor;
 
 vec4 tint(vec2 vUV) { // main
     
-    vec4 input = texture2D(_input_, vUV);
-    vec3 tintedRGB = vec3(0,1,0); //mix(input.rgb, tintColor, intensity);
-    return vec4(tintedRGB, input.a);
+    vec4 textureColor = texture2D(_input_, vUV);
+    vec3 tintedRGB = mix(textureColor.rgb, tintColor, intensity);
+    return vec4(tintedRGB, textureColor.a);
 }
